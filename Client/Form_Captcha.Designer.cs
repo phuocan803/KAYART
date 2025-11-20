81a89d2005b9065a1f0a444c7649e9ce95a4a3d4
HaNguyenHieuAn <24520043@ms.uit.edu.vn>
2025-11-21 00:18:38 +0700

    feat(security): add captcha form designer

---
+++ Client/Form_Captcha.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace Client
+{
+    partial class Form_Captcha
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            webView21.Location = new System.Drawing.Point(0, 0);
+            webView21.Name = "webView21";
+            webView21.Size = new System.Drawing.Size(334, 526);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            webView21.Click += webView21_Click;
+            // 
+            // Form_Captcha
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(334, 526);
+            Controls.Add(webView21);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Name = "Form_Captcha";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "reCaptcha";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+        }
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
