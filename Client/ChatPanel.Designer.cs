ed1547f6f5c9087de8dbb59ac0dfcd2976e7d793
annguyen020106 <annguyen020106@gmail.com>
2025-11-26 01:26:04 +0700

    feat(chat): add chat panel designer

---
+++ Client/ChatPanel.Designer.cs
@@ -0,0 +1,15 @@
+namespace Client
+{
+    partial class ChatPanel
+    {
+        private System.ComponentModel.IContainer components = null;
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            Name = "ChatPanel";
+            Size = new System.Drawing.Size(362, 703);
+            ResumeLayout(false);
+        }
+
+    }
+}
